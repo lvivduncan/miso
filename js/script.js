f7930595c8033b354fc0ce5134b7eacc75932259
lvivduncan <lvivduncan@gmail.com>
2021-12-16 23:44:39 +0200

    testimonials

--- js/script.js
+++ js/script.js
@@ -13,3 +13,96 @@ window.addEventListener('scroll', () => {
         }, 10)
     }
 })
+
+// https://github.com/lvivduncan/levus-horizontal-scroll
+{
+    const scroll = document.querySelectorAll('.levus-horizontal-scroll')
+
+    if(scroll.length > 0){
+
+        scroll.forEach(item => {
+
+            const ul = item.querySelector('ul')
+
+            // elements
+            let li = ul.querySelectorAll('li')
+
+            // if less than 4, cloned 
+            if (li.length <= 3) {
+
+                // cloned and append elements
+                li.forEach(element => ul.append(element.cloneNode(true)))
+
+                // new nodelist
+                li = item.querySelectorAll('li')
+            }
+/* 
+            item.innerHTML += '<div class="levus-nav"><span class="left"></span><span class="right"></span></div>'
+
+            item.addEventListener('click', event => {
+
+                const ul = item.querySelector('ul')
+
+                if (event.target.className == 'left') {
+
+                    // move last element
+                    const last = ul.lastElementChild
+                    ul.prepend(last)
+
+                    // destroy transition
+                    ul.style.transition = 'none'
+                    ul.classList.add('to-right')
+
+                    setTimeout(() => {
+
+                        ul.classList.remove('to-right')
+                        ul.style.transition = '.5s'
+                    }, 50)
+                }
+            })
+
+            item.addEventListener('click', event => {
+
+                const ul = item.querySelector('ul')
+
+                if (event.target.className == 'right') {
+
+                    // move first element
+                    const first = ul.firstElementChild
+                    ul.append(first)
+
+                    // destroy transition
+                    ul.style.transition = 'none'
+                    ul.classList.add('to-left')
+
+                    setTimeout(() => {
+
+                        ul.classList.remove('to-left')
+                        ul.style.transition = '.5s'
+                    }, 50)
+                }
+            }) */
+
+            setInterval(() => {
+                const ul = item.querySelector('ul')
+    
+                // move first element
+                const first = ul.firstElementChild
+                ul.append(first)
+    
+                // destroy transition
+                ul.style.transition = 'none'
+                ul.classList.add('to-left')
+    
+                setTimeout(() => {
+
+                    ul.classList.remove('to-left')
+                    ul.style.transition = '.5s'
+
+                }, 50)
+             
+            }, 7000)
+
+        })
+    }
+}
