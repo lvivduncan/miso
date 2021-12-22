046331fb70aceb1a836d64158b997b21f7ad7063
lvivduncan <lvivduncan@gmail.com>
2021-12-22 23:40:40 +0200

    finish: menu, mobile menu, gallery, testimonials, address

--- js/script.js
+++ js/script.js
@@ -14,6 +14,28 @@ window.addEventListener('scroll', () => {
     }
 })
 
+// плавний скрол
+// https://webdesign.tutsplus.com/tutorials/smooth-scrolling-vanilla-javascript--cms-35165
+{
+    const links = document.querySelectorAll("#banner ul a")
+
+    for (const link of links) {
+        link.addEventListener("click", clickHandler)
+    }
+
+    function clickHandler(event) {
+        event.preventDefault()
+
+        const href = this.getAttribute("href")
+        const offsetTop = document.querySelector(href).offsetTop
+
+        scroll({
+            top: offsetTop,
+            behavior: "smooth"
+        })
+    }
+}
+
 // https://github.com/lvivduncan/levus-horizontal-scroll
 {
     const scroll = document.querySelectorAll('.levus-horizontal-scroll')
@@ -83,18 +105,136 @@ document.getElementById('menu').addEventListener('click', () => {
 
     (new Fancybox([
         {
-          src: '<nav><ul><li><a href="#rolls">Роли</a></li><li><a href="#sashimi">Сашімі</a></li><li><a href="#nigiri">Нігірі</a></li><li><a href="#miso-soup">Місо-суп</a></li></ul><ul><li><a href="#">Про нас</a></li><li><a href="#">Галерея</a></li><li><a href="#testimonials">Відгуки</a></li><li><a href="#">Контакти</a></li></ul></nav>',
+        src: `
+            <nav id="modal">
+                <ul>
+                    <li>
+                        <a href="#rolls">
+                            <picture>                            
+                                <img src="temp/rolls.png" alt="">
+                            </picture>
+                            Філадельфія
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#sashimi">
+                            <picture>
+                                <img src="temp/sashimi.png" alt="">
+                            </picture>
+                            Дракони
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#hosomaki">
+                            <picture>
+                                <img src="temp/hosomaki.png" alt="">
+                            </picture>
+                            Хосомаки
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#nigiri">
+                            <picture>
+                                <img src="temp/nigiri.png" alt="">
+                            </picture>
+                            Нігірі
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#drink">
+                            <picture>
+                                <img src="temp/drink.png" alt="">
+                            </picture>
+                            Напої
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#box-set">
+                            <picture>
+                                <img src="temp/set.png" alt="">
+                            </picture>
+                            Бокс сети
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#miso-soup">
+                            <picture>
+                                <img src="temp/ramen.png" alt="">
+                            </picture>
+                            Місо-суп
+                        </a>
+                    </li>
+                </ul>
+            </nav>`,
+        type: "html",
+        },
+    ]))
+
+})
+
+// мобільне меню телефонів
+document.getElementById('phone').addEventListener('click', () => {
+
+    (new Fancybox([
+        {
+        src: '<nav id="call"><ul><li><a href="tel:0123456789">0123456789</a></li></ul></nav>',
+        type: "html",
+        },
+    ]))
+
+})
+
+/*
+// клік на кошикові відкриває модальне вікно зі замовленням
+// у вікні можна змінити кількість або видалити і
+// одразу оформити або ж закрити і замовити ще щось
+*/
+
+document.getElementById('basket').addEventListener('click', () => {
+
+    (new Fancybox([
+        {
+          src: `<div id="form">
+          <div id="goods">
+              <ul>
+                  <li>Назва замовлення 1</li>
+                  <li>Назва замовлення 2</li>
+                  <li>Назва замовлення 3</li>
+                  <li>Назва замовлення 4</li>
+                  <li>Назва замовлення 5</li>
+                  <li>Назва замовлення 6</li>
+                  <li>Назва замовлення 7</li>
+              </ul>                    
+          </div>
+
+          <p>Оформити замовлення?</p>
+
+          <form>
+              <input type="text" placeholder="ім'я">
+              <input type="number" placeholder="телефон">
+              <textarea placeholder="коментар"></textarea>
+              <label>
+                  <input type="radio" name="radio" checked> забрати самостійно 
+              </label>
+              <label>
+                  <input type="radio" name="radio"> потрібна доставка (+200грн)
+              </label>
+              <select name="" id="">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+              </select>
+              <button>Оформити замовлення</button>
+          </form>
+      </div>`,
           type: "html",
         },
         
-      ]))
+    ]))    
 })
-/* 
-// basket
-const products = document.querySelectorAll('.menu article')
-
-for(let i = 0; i<products.length; i++)
-{
-
-
-} */
