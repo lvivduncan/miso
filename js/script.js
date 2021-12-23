773344aa2eaf02e2dd4391b52ef2eb6e541d04b2
lvivduncan <lvivduncan@gmail.com>
2021-12-23 23:46:31 +0200

    basket -- finish is comming

--- js/script.js
+++ js/script.js
@@ -111,7 +111,7 @@ document.getElementById('menu').addEventListener('click', () => {
                     <li>
                         <a href="#rolls">
                             <picture>                            
-                                <img src="temp/rolls.png" alt="">
+                                <img src="images/rolls.png" alt="">
                             </picture>
                             Філадельфія
                         </a>
@@ -120,7 +120,7 @@ document.getElementById('menu').addEventListener('click', () => {
                     <li>
                         <a href="#sashimi">
                             <picture>
-                                <img src="temp/sashimi.png" alt="">
+                                <img src="images/sashimi.png" alt="">
                             </picture>
                             Дракони
                         </a>
@@ -129,7 +129,7 @@ document.getElementById('menu').addEventListener('click', () => {
                     <li>
                         <a href="#hosomaki">
                             <picture>
-                                <img src="temp/hosomaki.png" alt="">
+                                <img src="images/hosomaki.png" alt="">
                             </picture>
                             Хосомаки
                         </a>
@@ -138,7 +138,7 @@ document.getElementById('menu').addEventListener('click', () => {
                     <li>
                         <a href="#nigiri">
                             <picture>
-                                <img src="temp/nigiri.png" alt="">
+                                <img src="images/nigiri.png" alt="">
                             </picture>
                             Нігірі
                         </a>
@@ -147,7 +147,7 @@ document.getElementById('menu').addEventListener('click', () => {
                     <li>
                         <a href="#drink">
                             <picture>
-                                <img src="temp/drink.png" alt="">
+                                <img src="images/drink.png" alt="">
                             </picture>
                             Напої
                         </a>
@@ -156,7 +156,7 @@ document.getElementById('menu').addEventListener('click', () => {
                     <li>
                         <a href="#box-set">
                             <picture>
-                                <img src="temp/set.png" alt="">
+                                <img src="images/set.png" alt="">
                             </picture>
                             Бокс сети
                         </a>
@@ -165,7 +165,7 @@ document.getElementById('menu').addEventListener('click', () => {
                     <li>
                         <a href="#miso-soup">
                             <picture>
-                                <img src="temp/ramen.png" alt="">
+                                <img src="images/ramen.png" alt="">
                             </picture>
                             Місо-суп
                         </a>
@@ -183,58 +183,141 @@ document.getElementById('phone').addEventListener('click', () => {
 
     (new Fancybox([
         {
-        src: '<nav id="call"><ul><li><a href="tel:0123456789">0123456789</a></li></ul></nav>',
+        src: '<nav id="call"><ul><li><a href="tel:0731305870">(073) 130-58-70</a></li><li><a href="tel:0986533151">(098) 65-331-51</a></li></ul></nav>',
         type: "html",
         },
     ]))
 
 })
 
+
 /*
 // клік на кошикові відкриває модальне вікно зі замовленням
 // у вікні можна змінити кількість або видалити і
 // одразу оформити або ж закрити і замовити ще щось
+// усе пишеться у локалсторедж
 */
 
-document.getElementById('basket').addEventListener('click', () => {
+// кошик, який по дефолту прихований
+const basket = document.getElementById('basket')
+
+// кількість товарів у кошикові
+const span = basket.querySelector('span')
+
+// відкриваємо форму оформлення замовлення по кліку на кошик
+basket.addEventListener('click', viewOrderForm)
+
+// об'єкти для даних, що будуть писатися у базу
+let names = []
+let prices = []
+
+// показуємо/ховаємо кошик
+toggleBasket()
+
+// усі товари
+const articles = document.querySelectorAll('.menu article')
+
+for(let i = 0; i < articles.length; i++){
+
+    const product = articles[i]
+
+    // клік на продукт додає його у базу і підсвічує
+    product.addEventListener('click', () => {
+
+        // підсвітка номінальна, не зберігається після оновлення
+        product.classList.add('active')
+
+
+
+
+
+        // чек бази. дописуємо, якщо не пуста
+        if(localStorage.getItem('names') !== null){
+
+            names = JSON.parse(localStorage.getItem('names')).split(';')
+            prices = JSON.parse(localStorage.getItem('prices')).split(';')
+
+            // додаємо назву
+            names.push(product.querySelector('h1').textContent)
+
+            // додаємо ціну
+            prices.push(product.querySelector('span').textContent)
+
+            // пишемо у базу
+            localStorage.setItem('names', JSON.stringify(names.join(';')))
+            localStorage.setItem('prices', JSON.stringify(prices.join(';')))           
+
+        } else {
+
+            // додаємо назву
+            names.push(product.querySelector('h1').textContent)
+
+            // додаємо ціну
+            prices.push(product.querySelector('span').textContent)
+
+            // пишемо у базу
+            localStorage.setItem('names', JSON.stringify(names.join(';')))
+            localStorage.setItem('prices', JSON.stringify(prices.join(';')))        
+        }
+
+        // показуємо кошик
+        toggleBasket()        
+    })
+}
+
+// якщо у базі уже щось лежить -- показати кошик
+function toggleBasket(){
+
+    localStorage.getItem('names') ? basket.classList.add('active') : basket.classList.remove('active')
+}
+
+// отримуємо дані з бази і обробляємо
+function goodsFromBase(){
+
+    const names = JSON.parse(localStorage.getItem('names')).split(';')
+    const prices = JSON.parse(localStorage.getItem('prices')).split(';')
+
+    let output = ''
+
+    for(let i = 0; i<names.length; i++){
+
+        output += `
+        <li id="${i}">
+            <i class="minus">-</i>
+            ${names[i]} <span>${prices[i]}</span>
+            <i class="plus">+</i>
+        <li>`
+    }
+
+    return `<ul>${output}</ul>`
+}
+
+// показати форму оформлення
+function viewOrderForm(){
 
     (new Fancybox([
         {
           src: `<div id="form">
-          <div id="goods">
-              <ul>
-                  <li>Назва замовлення 1</li>
-                  <li>Назва замовлення 2</li>
-                  <li>Назва замовлення 3</li>
-                  <li>Назва замовлення 4</li>
-                  <li>Назва замовлення 5</li>
-                  <li>Назва замовлення 6</li>
-                  <li>Назва замовлення 7</li>
-              </ul>                    
-          </div>
+            <div id="goods">${goodsFromBase()}</div>
 
-          <p>Оформити замовлення?</p>
+            <p>Оформити замовлення?</p>
 
-          <form>
-              <input type="text" placeholder="ім'я">
-              <input type="number" placeholder="телефон">
-              <textarea placeholder="коментар"></textarea>
-              <label>
-                  <input type="radio" name="radio" checked> забрати самостійно 
-              </label>
-              <label>
-                  <input type="radio" name="radio"> потрібна доставка (+200грн)
-              </label>
-              <select name="" id="">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-              </select>
-              <button>Оформити замовлення</button>
-          </form>
-      </div>`,
+            <form>
+                <input type="text" placeholder="ім'я">
+                <input type="number" placeholder="телефон">
+                <textarea placeholder="коментар"></textarea>
+                <label>
+                    <input type="radio" name="radio" checked> забрати самостійно 
+                </label>
+                <label>
+                    <input type="radio" name="radio"> потрібна доставка (+200грн)
+                </label>
+                <input type="number" min="10" max="21" placeholder="10:00">
+                <button>Оформити замовлення</button>
+            </form>
+        </div>`,
           type: "html",
         },
         
-    ]))    
-})
+    ]))  
+}
